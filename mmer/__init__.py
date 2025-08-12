60947c3b49ae9af926baa26a4fd1e5116c75efea
Sajad Hussaini <hussaini.smsajad@gmail.com>
2025-08-12 13:28:49 +0100

    fixed init no visual

--- mmer/__init__.py
+++ mmer/__init__.py
@@ -1,7 +1,5 @@
 from .core.mixed_effect import MixedEffectRegressor
 from .core.mixed_result import MixedEffectResults
-from .visual.style import style
-from .visual import plots
 
 __version__ = '0.1.0'
-__all__ = ['MixedEffectRegressor', 'MixedEffectResults', 'style', 'plots']
+__all__ = ['MixedEffectRegressor', 'MixedEffectResults']
