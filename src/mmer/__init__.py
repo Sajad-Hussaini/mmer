b9c812a0981217073c122893d3d14df5519a311f
Sajad Hussaini <hussaini.smsajad@gmail.com>
2026-04-16 22:30:05 +0100

    v1.1.2 added woodburry solver, speed up

--- src/mmer/__init__.py
+++ src/mmer/__init__.py
@@ -2,5 +2,5 @@ from .core.mixed_effect import MixedEffectRegressor
 from .core.mixed_result import MixedEffectResults
 from .utils.multi_output import MultiOutputRegressor
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 __all__ = ['MixedEffectRegressor', 'MixedEffectResults', 'MultiOutputRegressor']
